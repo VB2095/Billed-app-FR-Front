ecc50b5c5f35e4c9b946d8ebff21709416ee2163
Victor Bross <bross.victor@gmail.com>
2023-02-13 12:13:27 +0100

    Change handleShowTicket function to fix bug on dashboard

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -130,6 +130,28 @@ export default class {
     this.onNavigate(ROUTES_PATH['Dashboard'])
   }
 
+  // handleShowTickets(e, bills, index) {
+  //   if (this.counter === undefined || this.index !== index) this.counter = 0
+  //   if (this.index === undefined || this.index !== index) this.index = index
+  //   if (this.counter % 2 === 0) {
+  //     $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
+  //     $(`#status-bills-container${this.index}`)
+  //       .html(cards(filteredBills(bills, getStatus(this.index))))
+  //     this.counter ++
+  //   } else {
+  //     $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
+  //     $(`#status-bills-container${this.index}`)
+  //       .html("")
+  //     this.counter ++
+  //   }
+
+  //   bills.forEach(bill => {
+  //     $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+  //   })
+
+  //   return bills
+
+  // }
   handleShowTickets(e, bills, index) {
     if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
@@ -152,7 +174,6 @@ export default class {
       this.counter ++
     }
   }
-
   getBillsAllUsers = () => {
     if (this.store) {
       return this.store
